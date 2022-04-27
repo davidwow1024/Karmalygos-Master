f5311c4ec47b1700f7beab6b7a8d1de16d1088bb
◄◄⚔ϨϾ∆₹₣∆ϾƩ⚔►► <50658729+mikefernandz@users.noreply.github.com>
2022-04-27 16:47:28 -0700

    Core*/Console Log KarMalygos

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -131,14 +131,16 @@ extern int main(int argc, char** argv)
     TC_LOG_INFO("server.authserver", "%s (authserver)", _FULLVERSION);
     TC_LOG_INFO("server.authserver", "<Ctrl-C> to stop.\n");
     
-    TC_LOG_INFO("server.authserver", "   ______  __  __  __  __  ______ __  ______  ______ ");
-    TC_LOG_INFO("server.authserver", "  /\\  ___\\/\\ \\/ / /\\ \\_\\ \\/\\  ___/\\ \\/\\  == \\/\\  ___\\ ");
-    TC_LOG_INFO("server.authserver", "  \\ \\___  \\ \\  _'-\\ \\____ \\ \\  __\\ \\ \\ \\  __<\\ \\  __\\ ");
-    TC_LOG_INFO("server.authserver", "   \\/\\_____\\ \\_\\ \\_\\/\\_____\\ \\_\\  \\ \\_\\ \\_\\ \\_\\ \\_____\\ ");
-    TC_LOG_INFO("server.authserver", "    \\/_____/\\/_/\\/_/\\/_____/\\/_/   \\/_/\\/_/ /_/\\/_____/ ");
-    TC_LOG_INFO("server.authserver", "  Project SkyFireEmu 2014(c) Open-sourced Game Emulation ");
-    TC_LOG_INFO("server.authserver", "           <http://www.projectskyfire.org/> \n");
-
+	TC_LOG_INFO("server.authserver", "                                                                                     ");
+    TC_LOG_INFO("server.authserver", " ██╗░░██╗░█████╗░██████╗░███╗░░░███╗░█████╗░██╗░░░░░██╗░░░██╗░██████╗░░█████╗░░██████╗");
+    TC_LOG_INFO("server.authserver", " ██║░██╔╝██╔══██╗██╔══██╗████╗░████║██╔══██╗██║░░░░░╚██╗░██╔╝██╔════╝░██╔══██╗██╔════╝");
+    TC_LOG_INFO("server.authserver", " █████═╝░███████║██████╔╝██╔████╔██║███████║██║░░░░░░╚████╔╝░██║░░██╗░██║░░██║╚█████╗░");
+    TC_LOG_INFO("server.authserver", " ██╔═██╗░██╔══██║██╔══██╗██║╚██╔╝██║██╔══██║██║░░░░░░░╚██╔╝░░██║░░╚██╗██║░░██║░╚═══██╗");
+    TC_LOG_INFO("server.authserver", " ██║░╚██╗██║░░██║██║░░██║██║░╚═╝░██║██║░░██║███████╗░░░██║░░░╚██████╔╝╚█████╔╝██████╔╝");
+    TC_LOG_INFO("server.authserver", " ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░░╚═════╝░░╚════╝░╚═════╝░");
+	TC_LOG_INFO("server.authserver", " Project KarMalygosEmu 2014(c) Private-sourced Game Emulation ");
+       TC_LOG_INFO("server.authserver", "<http://www.projectskarmalygos.org/> \n");
+	   
     TC_LOG_INFO("server.authserver", "Using configuration file %s.", configFile);
 
     TC_LOG_WARN("server.authserver", "%s (Library: %s)", OPENSSL_VERSION_TEXT, SSLeay_version(SSLEAY_VERSION));
