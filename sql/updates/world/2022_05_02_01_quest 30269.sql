0ec3e3f2f8e9a9f8e1e58f9e0d43b71abedb4018
◄◄⚔ϨϾ∆₹₣∆ϾƩ⚔►► <50658729+mikefernandz@users.noreply.github.com>
2022-05-03 02:12:46 -0700

    DB*/Fix_syntax

--- sql/updates/world/2022_05_02_01_quest 30269.sql
+++ sql/updates/world/2022_05_02_01_quest 30269.sql
@@ -1,4 +1,4 @@
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=13468 AND `SourceEntry`=0 AND `SourceId`=0 AND `ElseGroup`=0ñ
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=13468 AND `SourceEntry`=0 AND `SourceId`=0 AND `ElseGroup`=0;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
 (15, 13468, 0, 0, 0, 9, 0, 30269, 0, 0, 0, 0, 0, '', '');
 
