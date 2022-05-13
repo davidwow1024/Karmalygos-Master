eea58a15ab2da1945ad118e6c13958559ce810cc
SargeroDeV <neohd251535@gmail.com>
2022-05-13 18:49:18 -0500

    DB: Fix Twilight Highlands terrain_swamp http://10.96.65.5/Bugtracker/en/bugtracker/report/2

---
+++ sql/updates/world/2022_05_13_02_Twilight_Highlands_terrain.sql
@@ -0,0 +1,2 @@
+DELETE FROM `terrain_swap_defaults` WHERE `MapId`=0 AND `TerrainSwapMap`=736;
+INSERT INTO `terrain_swap_defaults` (`MapId`, `TerrainSwapMap`, `Comment`) VALUES (0, 736, 'Twilight Highlands Dragonmaw Phase');
