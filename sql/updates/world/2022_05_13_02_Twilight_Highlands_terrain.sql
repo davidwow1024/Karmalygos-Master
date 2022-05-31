3476875a2e95fac1ddc7a90b2c443cbe437f6d89
◄◄⚔ϨϾ∆₹₣∆ϾƩ⚔►► <50658729+mikefernandz@users.noreply.github.com>
2022-05-31 16:42:41 -0700

    Revert "DB: Fix Twilight Highlands terrain_swamp http://10.96.65.5/Bugtracker/en/bugtracker/report/2"
    This reverts commit eea58a15ab2da1945ad118e6c13958559ce810cc.

--- sql/updates/world/2022_05_13_02_Twilight_Highlands_terrain.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `terrain_swap_defaults` WHERE `MapId`=0 AND `TerrainSwapMap`=736;
-INSERT INTO `terrain_swap_defaults` (`MapId`, `TerrainSwapMap`, `Comment`) VALUES (0, 736, 'Twilight Highlands Dragonmaw Phase');
