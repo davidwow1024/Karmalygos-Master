792f9a1f55147d0b8a043812e5ff070ea0eb6da8
Syrus23 <96646593+Syrus23@users.noreply.github.com>
2022-06-05 01:17:28 -0400

    Core: Fixed up auras Speedbarge Diving Helm (75627 / 75651).
    Note* This is the aura that makes you swim faster in Thousand Needles.
    
    Report link: http://10.96.65.5/foro/index.php?threads/81/

--- src/server/scripts/Kalimdor/zone_thousand_needles.cpp
+++ src/server/scripts/Kalimdor/zone_thousand_needles.cpp
@@ -18,12 +18,23 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "ScriptMgr.h"
+#include "SpellScript.h"
+#include "Unit.h"
+
 enum FreezingPipeType
 {
     NPC_FREEZING_PIPE_BUNNY_1 = 47423,
     NPC_FREEZING_PIPE_BUNNY_2 = 47424,
     NPC_FREEZING_PIPE_BUNNY_3 = 47425,
     NPC_FREEZING_PIPE_BUNNY_4 = 47426,
+};
+
+
+enum SpellsThousandsNeedles
+{
+	SPELL_SPEEDBARGE_DIVING_HELM_CHECK = 75651,
+	SPELL_SPEEDBARGE_DIVING_HELM_EFFECT = 75627,
 };
 
 // I-Scream Cryocannon 88452
@@ -50,7 +61,98 @@ class spell_thousand_needles_i_scream_cryocannon : public SpellScript
     }
 };
 
+class spell_thousand_needles_speedbarge_diving_helm_check : public SpellScriptLoader
+{
+public:
+	spell_thousand_needles_speedbarge_diving_helm_check() : SpellScriptLoader("spell_thousand_needles_speedbarge_diving_helm_check") { }
+
+	class spell_thousand_needles_speedbarge_diving_helm_check_SpellScript : public AuraScript
+	{
+	public:
+		spell_thousand_needles_speedbarge_diving_helm_check_SpellScript() : AuraScript() { checkTimer = 1000; }
+
+		PrepareAuraScript(spell_thousand_needles_speedbarge_diving_helm_check_SpellScript);
+
+		uint32 checkTimer;
+
+		void OnUpdate(uint32 p_Diff)
+		{
+			if (checkTimer <= p_Diff)
+			{
+				if (Unit * caster = GetCaster())
+					if (caster->IsInWater())
+					{
+						caster->RemoveAurasDueToSpell(SPELL_SPEEDBARGE_DIVING_HELM_CHECK);
+						caster->AddAura(SPELL_SPEEDBARGE_DIVING_HELM_EFFECT, caster);
+					}
+
+				checkTimer = 1000;
+			}
+			else
+				checkTimer -= p_Diff;
+		}
+
+	private:
+
+		void Register() override
+		{
+			OnAuraUpdate += AuraUpdateFn(spell_thousand_needles_speedbarge_diving_helm_check_SpellScript::OnUpdate);
+		}
+	};
+	AuraScript* GetAuraScript() const override
+	{
+		return new spell_thousand_needles_speedbarge_diving_helm_check_SpellScript();
+	}
+};
+
+class spell_thousand_needles_speedbarge_diving_helm_effect : public SpellScriptLoader
+{
+public:
+	spell_thousand_needles_speedbarge_diving_helm_effect() : SpellScriptLoader("spell_thousand_needles_speedbarge_diving_helm_effect") { }
+
+	class spell_thousand_needles_speedbarge_diving_helm_effect_SpellScript : public AuraScript
+	{
+	public:
+		spell_thousand_needles_speedbarge_diving_helm_effect_SpellScript() : AuraScript() { checkTimer = 1000; }
+
+		PrepareAuraScript(spell_thousand_needles_speedbarge_diving_helm_effect_SpellScript);
+
+		uint32 checkTimer;
+
+		void OnUpdate(uint32 p_Diff)
+		{
+			if (checkTimer <= p_Diff)
+			{
+				if (Unit * caster = GetCaster())
+					if (!caster->IsInWater())
+					{
+						caster->RemoveAurasDueToSpell(SPELL_SPEEDBARGE_DIVING_HELM_EFFECT);
+						caster->AddAura(SPELL_SPEEDBARGE_DIVING_HELM_CHECK, caster);
+					}
+
+				checkTimer = 1000;
+			}
+			else
+				checkTimer -= p_Diff;
+		}
+
+	private:
+
+		void Register() override
+		{
+			OnAuraUpdate += AuraUpdateFn(spell_thousand_needles_speedbarge_diving_helm_effect_SpellScript::OnUpdate);
+		}
+	};
+	AuraScript* GetAuraScript() const override
+	{
+		return new spell_thousand_needles_speedbarge_diving_helm_effect_SpellScript();
+	}
+};
+
+
 void AddSC_thousand_needles()
 {
-    new spell_script<spell_thousand_needles_i_scream_cryocannon>("spell_thousand_needles_i_scream_cryocannon");
+	new spell_script<spell_thousand_needles_i_scream_cryocannon>("spell_thousand_needles_i_scream_cryocannon");
+	new spell_thousand_needles_speedbarge_diving_helm_check();
+	new spell_thousand_needles_speedbarge_diving_helm_effect();
 }
