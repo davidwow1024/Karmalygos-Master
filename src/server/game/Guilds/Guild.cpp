a53f10dfae6c9b4cdb7cfd8f8156535fe1fa4a82
◄◄⚔ϨϾ∆₹₣∆ϾƩ⚔►► <50658729+mikefernandz@users.noreply.github.com>
2022-06-13 14:37:16 -0700

    Core*/Packet Rework Guild::SendEventNewLeader Fix crash Guild::SendEventNewLeader+43  Guild.cpp line 2743

--- src/server/game/Guilds/Guild.cpp
+++ src/server/game/Guilds/Guild.cpp
@@ -2738,38 +2738,71 @@ void Guild::SendEventMOTD(WorldSession* session, bool broadcast)
 
 void Guild::SendEventNewLeader(Member* newLeader, Member* oldLeader, bool isSelfPromoted)
 {
+	ObjectGuid newLeaderGUID = newLeader ? newLeader->GetGUID() : 0;
 
+	std::string newLeaderName = newLeader ? newLeader->GetName() : "";
 
-	ObjectGuid gumGuid = oldLeader->GetGUID();
-	ObjectGuid newGumGuid = newLeader->GetGUID();
+	ObjectGuid oldLeaderGUID = oldLeader ? oldLeader->GetGUID() : 0;
 
-	WorldPacket data(SMSG_GUILD_EVENT_NEW_LEADER, oldLeader->GetName().size() + newLeader->GetName().size() + 2 * 8);
-	data.WriteGuidMask(newGumGuid, 4, 2, 7);
-	data.WriteBit(gumGuid[4]);
-	data.WriteBits(oldLeader->GetName().size(), 6);
-	data.WriteBit(gumGuid[0]);
-	data.WriteGuidMask(newGumGuid, 6, 3);
+	std::string oldLeaderName = oldLeader ? oldLeader->GetName() : "";
+
+	
+
+	WorldPacket data(SMSG_GUILD_EVENT_NEW_LEADER);
+
+	data.WriteGuidMask(newLeaderGUID, 4, 2, 7);
+
+	data.WriteBit(oldLeaderGUID[4]);
+
+	data.WriteBits(oldLeaderName.size(), 6);
+
+	data.WriteBit(oldLeaderGUID[0]);
+
+	data.WriteGuidMask(newLeaderGUID, 6, 3);
+
 	data.WriteBit(isSelfPromoted);
-	data.WriteGuidMask(newGumGuid, 1, 0);
-	data.WriteGuidMask(gumGuid, 1, 7, 3, 6, 2);
-	data.WriteBits(newLeader->GetName().size(), 6);
-	data.WriteBit(gumGuid[5]);
-	data.WriteBit(newGumGuid[5]);
+
+	data.WriteGuidMask(newLeaderGUID, 1, 0);
+
+	data.WriteGuidMask(oldLeaderGUID, 1, 7, 3, 6, 2);
+
+	data.WriteBits(newLeaderName.size(), 6);
+
+	data.WriteBit(oldLeaderGUID[5]);
+
+	data.WriteBit(newLeaderGUID[5]);
+
+
+
 	data.FlushBits();
 
-	data.WriteGuidBytes(newGumGuid, 5, 6);
-	data.WriteString(oldLeader->GetName());
-	data.WriteString(newLeader->GetName());
-	data.WriteGuidBytes(newGumGuid, 3, 4);
-	data << (int32)realmID;
-	data.WriteByteSeq(gumGuid[6]);
-	data.WriteByteSeq(newGumGuid[0]);
-	data.WriteByteSeq(gumGuid[5]);
-	data.WriteGuidBytes(newGumGuid, 2, 7);
-	data.WriteGuidBytes(gumGuid, 7, 4);
-	data << (int32)realmID;
-	data.WriteByteSeq(newGumGuid[1]);
-	data.WriteGuidBytes(gumGuid, 2, 1, 3, 0);
+
+
+	data.WriteGuidBytes(newLeaderGUID, 5, 6);
+
+	data.WriteString(oldLeaderName);
+
+	data.WriteString(newLeaderName);
+
+	data.WriteGuidBytes(newLeaderGUID, 3, 4);
+
+	data << uint32(realmID);
+
+	data.WriteByteSeq(oldLeaderGUID[6]);
+
+	data.WriteByteSeq(newLeaderGUID[0]);
+
+	data.WriteByteSeq(oldLeaderGUID[5]);
+
+	data.WriteGuidBytes(newLeaderGUID, 2, 7);
+
+	data.WriteGuidBytes(oldLeaderGUID, 7, 4);
+
+	data << uint32(realmID);
+
+	data.WriteByteSeq(newLeaderGUID[1]);
+
+	data.WriteGuidBytes(oldLeaderGUID, 2, 1, 3, 0);
 
 	BroadcastPacket(&data);
 
