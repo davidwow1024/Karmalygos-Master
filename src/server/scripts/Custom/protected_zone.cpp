f75864b15f7e33a41cd16158faafe097e51be664
SargeroDeV <asd@asd.com>
2022-06-24 22:34:32 +0000

    Core: Block zones: Timeless Isle, Isle of Giants and Isle of Thunder.

--- src/server/scripts/Custom/protected_zone.cpp
+++ src/server/scripts/Custom/protected_zone.cpp
@@ -2,11 +2,9 @@
 
 enum ForbiddenAreas : uint16
 {
-	AREA_TIMELESS_ISLE			= 6757,	// Timeless Isle
-	AREA_ISLE_OF_GIANT			= 6661, // IsleofGiants
-	AREA_ISLE_OF_THUNDER		= 6507, // IsleofThunder
-
-	MAP_MIST_OF_PANDARIA	    = 870,
+	ZONE_TIMELESS_ISLE			= 6757,	// Timeless Isle
+	ZONE_ISLE_OF_GIANT			= 6661, // IsleofGiants
+	ZONE_ISLE_OF_THUNDER		= 6507, // IsleofThunder
 };
      
  class protected_zone : public PlayerScript
@@ -14,47 +12,30 @@ enum ForbiddenAreas : uint16
  public:
      protected_zone() : PlayerScript("protected_zone") {}
      
-     void OnMapChanged(Player* player)
-     {
-		if (player->GetMapId() == MAP_MIST_OF_PANDARIA)
-		{ 
-			uint64 mod = player->GetMoney() * 0.1f;
-
-			if (player->GetSession()->GetSecurity() <= 1)
-			{
-				if (player->GetTeamId() == TEAM_ALLIANCE)
-					player->TeleportTo(0, -8833.07f, 622.778f, 93.9317f, 0.6771f);
-				else if (player->GetTeamId() == TEAM_HORDE)
-					player->TeleportTo(1, 1569.97f, -4397.41f, 16.0472f, 0.543025f);
-
-				player->SetMoney(player->GetMoney() - player->GetMoney() * 0.1f);
-				ChatHandler(player->GetSession()).PSendSysMessage("|cffFF0000Se le ha retirado|r %u |cffFF0000de oro por intentar entrar en una zona prohibida.|r", mod / 10000);
-			}
-		}
-     }
-	 /* For the future
-	 void OnUpdateZone(Player* player, uint32 newZone, uint32 newArea)
+     void OnUpdateZone(Player* player, uint32 newZone, uint32 /*newArea*/)
 	 {
 		 switch (newZone)
 		 {
-			 case AREA_TIMELESS_ISLE:
-			 case AREA_ISLE_OF_GIANT:
-			 case AREA_ISLE_OF_THUNDER:
+			 case ZONE_TIMELESS_ISLE:
+			 case ZONE_ISLE_OF_GIANT:
+			 case ZONE_ISLE_OF_THUNDER:
 			 {
 				 if (player->GetSession()->GetSecurity() >= 1)
 					 return;
+
+				 uint64 mod = player->GetMoney() * 0.1f;
 				 
-				 if (player->GetTeamId() == TEAM_ALLIANCE)
-					 player->TeleportTo(0, -8833.07f, 622.778f, 93.9317f, 0.6771f);
-				 else if (player->GetTeamId() == TEAM_HORDE)
-					 player->TeleportTo(1, 1569.97f, -4397.41f, 16.0472f, 0.543025f);
+				 player->GetTeamId() == TEAM_ALLIANCE ? player->TeleportTo(0, -8833.07f, 622.778f, 93.9317f, 0.6771f) : player->TeleportTo(1, 1569.97f, -4397.41f, 16.0472f, 0.543025f);
+
+				 player->SetMoney(player->GetMoney() - player->GetMoney() * 0.1f);
+				 ChatHandler(player->GetSession()).PSendSysMessage("|cffFF0000Se le ha retirado|r %u |cffFF0000de oro por intentar entrar en una zona prohibida.|r", mod / 10000);
 
 				 break;
 			 }
 			 default:
 				 break;
 		 }
-	 }*/
+	 }
  };
      
  void AddSC_protected_zone()
