487223fae58f76cb9181b417869dd233cbcc3ad4
SargeroDeV <asd@asd.com>
2022-07-03 23:52:58 +0000

    DB: Fix quest 30602 OscarXXX

--- sql/updates/world/2022_07_03_04_fix_power_leveling.sql
+++ sql/updates/world/2022_07_03_04_fix_power_leveling.sql
@@ -1,1 +1,5 @@
 UPDATE `creature_template` SET `mindmg`=7052, `maxdmg`=11989, `flags_extra`= 64 WHERE `entry` in (61305,61306,61307);
+
+
+-- 30602
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry` 59806;
