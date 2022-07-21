db13b06228253fcabd50a11826cb4f1c0323087c
◄◄⚔ϨϾ∆₹₣∆ϾƩ⚔►► <50658729+mikefernandz@users.noreply.github.com>
2022-07-20 18:01:49 -0700

    DB*/Fix sql File (Upss)

--- sql/updates/world/2022_07_20_00_Access_Requirements.sql
+++ sql/updates/world/2022_07_20_00_Access_Requirements.sql
@@ -1,10 +1,9 @@
-DELETE FROM `battleground_template` WHERE `id`=699;
-INSERT INTO `battleground_template` (`id`, `MinPlayersPerTeam`, `MaxPlayersPerTeam`, `MinLvl`, `MaxLvl`, `AllianceStartLoc`, `AllianceStartO`, `HordeStartLoc`, `HordeStartO`, `StartMaxDist`, `Weight`, `ScriptName`, `Comment`) VALUES (699, 5, 10, 90, 100, 4059, 1.59375, 4060, 4.70393, 30, 3, '', 'Temple of Kotmogu');
-DELETE FROM `battleground_template` WHERE `id`=708;
-INSERT INTO `battleground_template` (`id`, `MinPlayersPerTeam`, `MaxPlayersPerTeam`, `MinLvl`, `MaxLvl`, `AllianceStartLoc`, `AllianceStartO`, `HordeStartLoc`, `HordeStartO`, `StartMaxDist`, `Weight`, `ScriptName`, `Comment`) VALUES (708, 5, 10, 90, 100, 4062, 5.59277, 4061, 5.57851, 50, 3, '', 'Silvershard Mines');
-DELETE FROM `battleground_template` WHERE `id`=719;
-INSERT INTO `battleground_template` (`id`, `MinPlayersPerTeam`, `MaxPlayersPerTeam`, `MinLvl`, `MaxLvl`, `AllianceStartLoc`, `AllianceStartO`, `HordeStartLoc`, `HordeStartO`, `StartMaxDist`, `Weight`, `ScriptName`, `Comment`) VALUES (719, 0, 5, 90, 100, 4136, 0, 4137, 0, 0, 1, '', 'Tol\'viron Arena');
-DELETE FROM `battleground_template` WHERE `id`=754;
-INSERT INTO `battleground_template` (`id`, `MinPlayersPerTeam`, `MaxPlayersPerTeam`, `MinLvl`, `MaxLvl`, `AllianceStartLoc`, `AllianceStartO`, `HordeStartLoc`, `HordeStartO`, `StartMaxDist`, `Weight`, `ScriptName`, `Comment`) VALUES (754, 5, 15, 90, 100, 4487, 0.659077, 4486, 4.04895, 100, 2, '', 'Deepwind Gorge');
-DELETE FROM `battleground_template` WHERE `id`=757;
-INSERT INTO `battleground_template` (`id`, `MinPlayersPerTeam`, `MaxPlayersPerTeam`, `MinLvl`, `MaxLvl`, `AllianceStartLoc`, `AllianceStartO`, `HordeStartLoc`, `HordeStartO`, `StartMaxDist`, `Weight`, `ScriptName`, `Comment`) VALUES (757, 0, 5, 90, 100, 4534, 0, 4535, 0, 0, 1, '', 'The Tiger\'s Peak');
+UPDATE `access_requirement` SET `completed_achievement` = 6689 WHERE `mapId` = 996 AND (`difficulty` = "RAID_10MAN_HEROIC" OR `difficulty` = "RAID_25MAN_HEROIC");
+
+DELETE FROM `trinity_string` WHERE `entry` IN (15000, 15001, 15002, 15003, 15004, 15005);
+INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc8`) VALUES 
+(15000, "You must clear Terrace of Endless Spring on normal before progressing to Heroic difficulty.", "Вы должны пройти «Терасса Вечной Весны» в обычном режиме, прежде чем переходить на героический уровень сложности."),
+(15001, "You must complete Heart of Fear in normal difficulty.", "Вы должны пройти «Сердце Страха» в обычном режиме."),
+(15002, "You must complete Heart of Fear on heroic difficulty.", "Вы должны пройти «Сердце Страха» в героическом режиме."),
+(15003, "You must complete Mogu'shan Vaults in normal difficulty.", "Вы должны пройти «Подземелья Могу'шан» в обычном режиме."),
+(15004, "You must complete Mogu'shan Vaults on heroic difficulty.", "Вы должны пройти «Подземелья Могу'шан» в героическом режиме.");
